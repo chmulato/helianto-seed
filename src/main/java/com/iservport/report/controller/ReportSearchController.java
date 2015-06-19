59b48931c2eeb348503010ba1958df5c5cd16995
mauriciofernandesdecastro <mauricio@iservport.com>
2015-06-19 06:43:46 -0300

    Renomeia controlador.

---
+++ src/main/java/com/iservport/report/controller/ReportSearchController.java
@@ -0,0 +1,15 @@
+package com.iservport.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * controlador de projetos
+ * 
+ * @author mauriciofernandesdecastro
+ */
+@Controller
+@RequestMapping("/api/report")
+public class ReportSearchController {
+	
+}
