432f0df00e5071cf57030e955d018d65a33c1891
Mauricio Fernandes de Castro <mauricio@iservport.com>
2015-09-22 09:39:09 -0300

    Apresentação.

--- src/main/java/com/iservport/user/repository/UserJournalRepository.java
+++ src/main/java/com/iservport/user/repository/UserJournalRepository.java
@@ -1,10 +1,12 @@
 package com.iservport.user.repository;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 
 import org.helianto.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.iservport.user.domain.UserJournal;
 
@@ -19,5 +21,16 @@ public interface UserJournalRepository extends JpaRepository<UserJournal, Serial
 	 * Find by natural key.
 	 */
 	UserJournal findByUserAndIssueDate(User user, Date issueDate);
+	
+	/**
+	 * Last user journal project check-in.
+	 */
+	@Query("select userJournal_.reportFolder.id "
+			+ "from UserJournal userJournal_ "
+			+ "where userJournal_.reportFolder.id in ?1 "
+			+ "and userJournal_.userJournalType = 'PRJ_CHECK_IN' "
+			+ "group by userJournal_.reportFolder.id "
+			+ "order by userJournal_.issueDate DESC ")
+	UserJournal findLastByProjectCheckIn(Collection<Integer> projectIds);
 
 }
