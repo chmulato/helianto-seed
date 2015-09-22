a3502493ee64aaa67500c5f9ffc929f5b9be5f56
Mauricio Fernandes de Castro <mauricio@iservport.com>
2015-09-22 15:02:02 -0300

    Contagem de diário.

--- src/main/java/com/iservport/user/repository/UserJournalRepository.java
+++ src/main/java/com/iservport/user/repository/UserJournalRepository.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 
+import org.helianto.core.internal.SimpleCounter;
 import org.helianto.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,12 +26,18 @@ public interface UserJournalRepository extends JpaRepository<UserJournal, Serial
 	/**
 	 * Last user journal project check-in.
 	 */
-	@Query("select userJournal_.reportFolder.id "
+	@Query("select new "
+			+ "org.helianto.core.internal.SimpleCounter("
+			+ "  userJournal_.reportFolder.id"
+			+ ", count(userJournal_.reportFolder.id)"
+			+ ", max(userJournal_.issueDate)"
+			+ ") "
 			+ "from UserJournal userJournal_ "
 			+ "where userJournal_.reportFolder.id in ?1 "
+			+ "and userJournal_.user.id = ?2 "
 			+ "and userJournal_.userJournalType = 'PRJ_CHECK_IN' "
 			+ "group by userJournal_.reportFolder.id "
 			+ "order by userJournal_.issueDate DESC ")
-	UserJournal findLastByProjectCheckIn(Collection<Integer> projectIds);
+	SimpleCounter findLastByProjectCheckIn(Collection<Integer> projectIds, int userId);
 
 }
